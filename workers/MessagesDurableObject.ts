200c6e92fc2e89fb354f17fd6941d0a2448ae3bb
Marko Jerkić <markojerkic266@gmail.com>
2025-06-08 22:39:54 +0200

    cp

--- workers/MessagesDurableObject.ts
+++ workers/MessagesDurableObject.ts
@@ -1,48 +1,45 @@
 import { DurableObject } from "cloudflare:workers";
+import type { AppLoadContext } from "react-router";
 
 export class MessagesDurableObject extends DurableObject {
-  currentlyConnectedWebSockets: number;
+  private connections: Set<WebSocket>;
 
-  constructor(ctx: DurableObjectState, env: Env) {
-    // This is reset whenever the constructor runs because
-    // regular WebSockets do not survive Durable Object resets.
-    //
-    // WebSockets accepted via the Hibernation API can survive
-    // a certain type of eviction, but we will not cover that here.
+  constructor(ctx: DurableObjectState, env: AppLoadContext["cloudflare"]) {
     super(ctx, env);
-    this.currentlyConnectedWebSockets = 0;
+
+    const websockets = this.ctx.getWebSockets();
+    this.connections = new Set<WebSocket>();
+
+    for (const ws of websockets) {
+      this.connections.add(ws);
+    }
   }
 
-  override fetch(request: Request): Response | Promise<Response> {
-    const webSocketPair = new WebSocketPair();
-    const [client, server] = Object.values(webSocketPair);
+  async fetch(req: Request) {
+    const websocketPair = new WebSocketPair();
+    const [client, server] = Object.values(websocketPair);
 
-    // Calling `accept()` tells the runtime that this WebSocket is to begin terminating
-    // request within the Durable Object. It has the effect of "accepting" the connection,
-    // and allowing the WebSocket to send and receive messages.
-    server.accept();
-    this.currentlyConnectedWebSockets += 1;
-
-    // Upon receiving a message from the client, the server replies with the same message,
-    // and the total number of connections with the "[Durable Object]: " prefix
-    server.addEventListener("message", (event: MessageEvent) => {
-      server.send(
-        `[Durable Object] currentlyConnectedWebSockets: ${this.currentlyConnectedWebSockets}`
-      );
-    });
-    server.send(
-      `[Durable Object] currentlyConnectedWebSockets: ${this.currentlyConnectedWebSockets}`
-    );
-
-    // If the client closes the connection, the runtime will close the connection too.
-    server.addEventListener("close", (cls: CloseEvent) => {
-      this.currentlyConnectedWebSockets -= 1;
-      server.close(cls.code, "Durable Object is closing WebSocket");
-    });
+    this.ctx.acceptWebSocket(server);
+    this.connections.add(client);
 
     return new Response(null, {
       status: 101,
       webSocket: client,
     });
+  }
+
+  webSocketError(ws: WebSocket, error: unknown) {
+    console.error("webSocketError", error);
+    this.connections.delete(ws);
+  }
+
+  webSocketClose(
+    ws: WebSocket,
+    _code: number,
+    _reason: string,
+    _wasClean: boolean
+  ) {
+    console.log("webSocketClose, connections", this.connections);
+    this.connections.delete(ws);
   }
 }
