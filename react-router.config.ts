52c394d696ac35cbccff5a42544512b33cdc624d
Marko Jerkić <markojerkic266@gmail.com>
2025-06-09 23:31:30 +0200

    cp

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
-declare module "react-router" {
-  interface Future {
-    unstable_middleware: true;
-  }
-}
+// declare module "react-router" {
+//   interface Future {
+//     unstable_middleware: true;
+//   }
+// }
 
 export default {
   ssr: true,
