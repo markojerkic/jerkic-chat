5097d68436b55b7728ca0597e2bb49b90eb33030
Marko Jerkić <markojerkic266@gmail.com>
2025-06-13 18:55:53 +0200

    tooltip is a bit wonky

--- app/components/sidebar-menu-item.tsx
+++ app/components/sidebar-menu-item.tsx
@@ -1,6 +1,21 @@
-import { Tooltip, TooltipContent } from "@radix-ui/react-tooltip";
-import { X } from "lucide-react"; // Assuming you have lucide-react installed for icons
-import { Link, useParams } from "react-router";
+import { X } from "lucide-react";
+import { Link, useFetcher, useParams } from "react-router";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+} from "~/components/ui/tooltip";
 import type { Route } from "../routes/+types/layout";
 import { Button } from "./ui/button";
 import { SidebarMenuItem } from "./ui/sidebar";
@@ -15,6 +30,7 @@ export type ThreadMenuItemProps = {
 
 export function ThreadMenuItem({ thread }: ThreadMenuItemProps) {
   const params = useParams<Route.ComponentProps["params"]>();
+  const fetcher = useFetcher();
 
   const isActive =
     params.threadId === thread.id ||
@@ -22,33 +38,57 @@ export function ThreadMenuItem({ thread }: ThreadMenuItemProps) {
       window.location.pathname.endsWith(thread.id));
 
   return (
-    <SidebarMenuItem className="group/menu-item relative flex items-center gap-2">
+    <SidebarMenuItem className="group/menu-item relative flex items-center gap-2 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:focus-visible:bg-sidebar-accent">
       <Link
         data-is-active={isActive}
         to={{ pathname: `/thread/${thread.id}` }}
-        className="group/link relative flex h-9 w-full items-center overflow-hidden rounded-lg p-2 py-1 text-sm outline-none hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:text-sidebar-accent-foreground focus-visible:ring-2 focus-visible:ring-sidebar-ring hover:focus-visible:bg-sidebar-accent data-[is-active=true]:bg-sidebar-accent data-[is-active=true]:text-sidebar-accent-foreground data-[is-active=true]:focus-visible:bg-sidebar-accent"
+        className="group/link relative flex h-9 w-full items-center overflow-hidden rounded-lg p-2 py-1 text-sm outline-none focus-visible:text-sidebar-accent-foreground focus-visible:ring-2 focus-visible:ring-sidebar-ring data-[is-active=true]:bg-sidebar-accent data-[is-active=true]:text-sidebar-accent-foreground data-[is-active=true]:focus-visible:bg-sidebar-accent"
       >
         <div className="min-w-0 flex-1 overflow-hidden text-ellipsis whitespace-nowrap">
           {thread.title ?? thread.id}
         </div>
+      </Link>
+      <div className="pointer-events-auto absolute top-0 right-1 bottom-0 z-50 flex translate-x-full items-center justify-end !p-1.5 text-muted-foreground transition-transform group-hover/menu-item:translate-x-0 group-hover/menu-item:bg-sidebar-accent">
+        <div className="pointer-events-none absolute top-0 right-[100%] bottom-0 h-full w-8 bg-gradient-to-l from-sidebar-accent to-transparent opacity-0 group-hover/link:opacity-100"></div>
+        <DeleteActionBtn />
+      </div>
+    </SidebarMenuItem>
+  );
+}
 
-        <div className="pointer-events-auto absolute top-0 right-1 bottom-0 z-50 flex translate-x-full items-center justify-end !p-1.5 text-muted-foreground transition-transform group-hover/link:translate-x-0 group-hover/link:bg-sidebar-accent">
-          <div className="pointer-events-none absolute top-0 right-[100%] bottom-0 h-full w-8 bg-gradient-to-l from-sidebar-accent to-transparent opacity-0 group-hover/link:opacity-100"></div>
-
-          <Tooltip>
-            <TooltipTrigger asChild>
+function DeleteActionBtn() {
+  return (
+    <TooltipProvider>
+      <AlertDialog>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <AlertDialogTrigger asChild>
               <Button
                 variant="ghost"
                 size="sm"
+                type="button"
                 className="h-[28px] w-[28px] p-1.5"
               >
                 <X className="size-4" />
               </Button>
-            </TooltipTrigger>
-            <TooltipContent>Delete thread</TooltipContent>
-          </Tooltip>
-        </div>
-      </Link>
-    </SidebarMenuItem>
+            </AlertDialogTrigger>
+          </TooltipTrigger>
+          <TooltipContent>Delete thread</TooltipContent>
+        </Tooltip>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </TooltipProvider>
   );
 }
