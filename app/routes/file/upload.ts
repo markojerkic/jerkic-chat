07adbf0f0b889609e31fe7ab308931a1b295e410
Marko Jerkić <markojerkic266@gmail.com>
2025-06-14 21:18:28 +0200

    add auth to uplaad

--- app/routes/file/upload.ts
+++ app/routes/file/upload.ts
@@ -1,4 +1,6 @@
+import { redirect } from "react-router";
 import * as v from "valibot";
+import { validateSession } from "~/server/auth/lucia";
 import type { Route } from "./+types/upload";
 
 const fileSchema = v.object({
@@ -8,6 +10,11 @@ const fileSchema = v.object({
 });
 
 export async function action({ request, context }: Route.ActionArgs) {
+  const session = await validateSession(context, request);
+  if (!session || !session.user) {
+    throw redirect("/auth/login");
+  }
+
   const formData = await request.formData();
   const file = v.parse(fileSchema, Object.fromEntries(formData.entries()));
 
