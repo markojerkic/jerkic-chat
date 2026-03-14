ef7d9bec5021ec2970d44e8b9d80ca8eebe5ac80
Marko Jerkić <markojerkic266@gmail.com>
2026-03-14 20:42:50 +0100

    title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,6 +28,7 @@ export const Route = createRootRouteWithContext<{}>()({
         href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
       },
     ],
+    meta: [{ title: "Jerkić.chat" }],
   }),
   component: RootComponent,
 });
