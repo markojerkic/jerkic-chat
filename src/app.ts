73ee1186cf66d95d3d441a4a03ca2a23033027eb
Marko Jerkić <markojerkic266@gmail.com>
2026-03-14 15:07:56 +0100

    setup request context

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,26 @@
 import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
 import { env } from "cloudflare:workers";
 import { drizzle, type DrizzleD1Database } from "drizzle-orm/d1";
-import { createRequestHandler } from "react-router";
 import * as schema from "../database/schema";
 
-declare module "react-router" {
-  export interface AppLoadContext {
-    cloudflare: {
-      env: Env;
-      ctx: ExecutionContext;
+type RequestContext = {
+  db: DrizzleD1Database<typeof schema>;
+};
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    server: {
+      requestContext: RequestContext;
     };
-    db: DrizzleD1Database<typeof schema>;
   }
 }
 
-// export { default } from "@tanstack/react-start/server-entry";
 export { MessagesDurableObject } from "./workers/MessagesDurableObject";
 
 export default createServerEntry({
-  fetch(request, opts) {
+  fetch(request) {
     const db = drizzle(env.DB, { schema });
 
-    return handler.fetch(request);
+    return handler.fetch(request, { context: { db } });
   },
 });
-
-// const requestHandler = createRequestHandler(
-//   () => import("virtual:react-router/server-build"),
-//   import.meta.env.MODE,
-// );
-//
-// export default {
-//   async fetch(request, env, ctx) {
-//     const db = drizzle(env.DB, { schema });
-//
-//     return requestHandler(request, {
-//       cloudflare: { env, ctx },
-//       db,
-//     });
-//   },
-// } satisfies ExportedHandler<Env>;
