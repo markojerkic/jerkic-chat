a4e61972582e90a1ef6595d31b7483533a3e4d9e
Marko Jerkić <markojerkic266@gmail.com>
2026-03-29 19:48:32 +0200

    delete not needed files

--- src/routes/_authenticated/thread.$threadId.ws.tsx
+++ src/routes/_authenticated/thread.$threadId.ws.tsx
@@ -5,7 +5,6 @@ export const Route = createFileRoute("/_authenticated/thread/$threadId/ws")({
   server: {
     handlers: {
       GET: async ({ request, params, context }) => {
-        console.log("hi", context);
         return await getWsConnection(
           request,
           context.currentUser.id,
