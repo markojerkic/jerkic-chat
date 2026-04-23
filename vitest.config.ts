d901c840812f48fa1eba45435f0cc1398ccce4ae
Marko Jerkić <markojerkic266@gmail.com>
2026-04-23 21:13:49 +0200

    smoke test works

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { cloudflareTest } from "@cloudflare/vitest-pool-workers";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart(),
     cloudflareTest({
       wrangler: { configPath: "./wrangler.jsonc" },
     }),
